34f493a4ab3be246f10c43bb0b5969846ef9c0eb
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-09-08 02:24:23 +0800

    Create NanaZipShellExtension project.

---
+++ NanaZipShellExtension/NanaZipShellExtension.cpp
@@ -0,0 +1,33 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZipShellExtension.cpp
+ * PURPOSE:   Implementation for NanaZip Shell Extension
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include <Mile.Windows.h>
+
+BOOL WINAPI DllMain(
+    _In_ HINSTANCE hinstDLL,
+    _In_ DWORD fdwReason,
+    _In_ LPVOID lpvReserved)
+{
+    UNREFERENCED_PARAMETER(hinstDLL);
+    UNREFERENCED_PARAMETER(lpvReserved);
+
+    switch (fdwReason)
+    {
+    case DLL_PROCESS_ATTACH:
+        break;
+    case DLL_THREAD_ATTACH:
+        break;
+    case DLL_THREAD_DETACH:
+        break;
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
