26f10770dbdd59850c79e7ec5866925c243bd296
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-09-25 12:00:26 +0800

    Update application icon.

--- SevenZip/CPP/7zip/UI/FileManager/AboutDialogRes.h
+++ SevenZip/CPP/7zip/UI/FileManager/AboutDialogRes.h
@@ -1,7 +1,8 @@
+#include "resourceGui.h"
+
 #define IDD_ABOUT  2900
 
 #define IDT_ABOUT_INFO  2901
 
-#define IDI_LOGO             100
 #define IDT_ABOUT_VERSION    101
 #define IDB_ABOUT_HOMEPAGE   110
