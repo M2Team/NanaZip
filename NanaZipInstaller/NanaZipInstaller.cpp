52221414df00bc61d30e91f69b5c36ec8ba0cb73
MouriNaruto <Mouri_Naruto@Outlook.com>
2021-11-20 23:49:07 +0800

    Add some design notes to NanaZipInstaller.

--- NanaZipInstaller/NanaZipInstaller.cpp
+++ NanaZipInstaller/NanaZipInstaller.cpp
@@ -21,6 +21,13 @@ int WINAPI wWinMain(
     UNREFERENCED_PARAMETER(lpCmdLine);
     UNREFERENCED_PARAMETER(nShowCmd);
 
+    // 纳纳齐普安装器预想设计
+    // 默认 自动判断，如果已安装则弹出配置（是否注册成全局应用，卸载）对话框
+    // --Install 安装 NanaZip （没有 --Source 参数则从 Microsoft Store 下载）
+    // --Source 本地 NanaZip 包
+    // --Global 将 NanaZip 注册成全局应用
+    // --Uninstall 卸载 NanaZip
+
     ::MessageBoxW(
         nullptr,
         L"Hello, NanaZipInstaller!\n",
