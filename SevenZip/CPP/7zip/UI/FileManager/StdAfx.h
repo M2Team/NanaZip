2d2dc43c1825f8369e667cfacbe390a73d33aec8
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-03-19 01:20:48 +0800

    Comment _WIN32_WINNT and WINVER macro definitions for resolving the compilation issue and provide chances for modernizing the implementations.

--- SevenZip/CPP/7zip/UI/FileManager/StdAfx.h
+++ SevenZip/CPP/7zip/UI/FileManager/StdAfx.h
@@ -7,8 +7,8 @@
    But now menu problem is fixed. So it's OK to use 0x0500 (Windows 2000) */
 
 // #define _WIN32_WINNT 0x0400
-#define _WIN32_WINNT 0x0500
-#define WINVER _WIN32_WINNT
+// #define _WIN32_WINNT 0x0500
+// #define WINVER _WIN32_WINNT
 
 #include "../../../Common/Common.h"
 
--- SevenZip/CPP/7zip/UI/GUI/StdAfx.h
+++ SevenZip/CPP/7zip/UI/GUI/StdAfx.h
@@ -4,8 +4,8 @@
 #define __STDAFX_H
 
 // #define _WIN32_WINNT 0x0400
-#define _WIN32_WINNT 0x0500
-#define WINVER _WIN32_WINNT
+// #define _WIN32_WINNT 0x0500
+// #define WINVER _WIN32_WINNT
 
 #include "../../../Common/Common.h"
 
