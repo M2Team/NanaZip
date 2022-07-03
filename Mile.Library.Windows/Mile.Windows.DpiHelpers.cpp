df620d0ca3abed32091a408f3c7280e2fa8e3155
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-07-03 19:46:53 +0800

    Add Mile.Library.Windows.

---
+++ Mile.Library.Windows/Mile.Windows.DpiHelpers.cpp
@@ -0,0 +1,82 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.DpiHelpers.cpp
+ * PURPOSE:   Implementation for Windows DPI Helpers
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#include "Mile.Windows.DpiHelpers.h"
+
+#include <cstring>
+
+namespace
+{
+    static bool IsPrivatePerMonitorSupportExtensionApplicable()
+    {
+        static bool CachedResult = ([]() -> bool
+        {
+            OSVERSIONINFOEXW OSVersionInfoEx;
+
+            // The private Per-Monitor DPI Awareness support extension is
+            // Windows 10 only.
+            std::memset(&OSVersionInfoEx, 0, sizeof(OSVERSIONINFOEXW));
+            OSVersionInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
+            OSVersionInfoEx.dwMajorVersion = 10;
+            if (!::VerifyVersionInfoW(
+                &OSVersionInfoEx,
+                VER_MAJORVERSION,
+                ::VerSetConditionMask(0, VER_MAJORVERSION, VER_GREATER_EQUAL)))
+            {
+                return false;
+            }
+
+            // We don't need the private Per-Monitor DPI Awareness support
+            // extension if the Per-Monitor (V2) DPI Awareness exists.
+            std::memset(&OSVersionInfoEx, 0, sizeof(OSVERSIONINFOEXW));
+            OSVersionInfoEx.dwOSVersionInfoSize = sizeof(OSVERSIONINFOEXW);
+            OSVersionInfoEx.dwBuildNumber = 14986;
+            if (::VerifyVersionInfoW(
+                &OSVersionInfoEx,
+                VER_BUILDNUMBER,
+                ::VerSetConditionMask(0, VER_BUILDNUMBER, VER_GREATER_EQUAL)))
+            {
+                return false;
+            }
+
+            return true;
+        }());
+
+        return CachedResult;
+    }
+}
+
+EXTERN_C BOOL WINAPI MileEnablePerMonitorDialogScaling()
+{
+    if (!::IsPrivatePerMonitorSupportExtensionApplicable())
+    {
+        return FALSE;
+    }
+
+    HMODULE ModuleHandle = ::GetModuleHandleW(L"user32.dll");
+    if (!ModuleHandle)
+    {
+        return FALSE;
+    }
+
+    // Reference: https://github.com/microsoft/terminal/blob
+    //            /9b92986b49bed8cc41fde4d6ef080921c41e6d9e
+    //            /src/interactivity/win32/windowdpiapi.cpp#L24
+    typedef BOOL(WINAPI* ProcType)();
+
+    ProcType ProcAddress = reinterpret_cast<ProcType>(
+        ::GetProcAddress(ModuleHandle, reinterpret_cast<LPCSTR>(2577)));
+    if (!ProcAddress)
+    {
+        return FALSE;
+    }
+
+    return ProcAddress();
+}
---
+++ Mile.Library.Windows/Mile.Windows.DpiHelpers.h
@@ -0,0 +1,36 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Windows.DpiHelpers.h
+ * PURPOSE:   Definition for Windows DPI Helpers
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
+ */
+
+#pragma once
+
+#ifndef MILE_WINDOWS_DPIHELPERS
+#define MILE_WINDOWS_DPIHELPERS
+
+#include <Windows.h>
+
+/**
+ * @brief Enables automatic display scaling of the dialogs in high-DPI
+ *        displays. Must be called before the creation of dialogs.
+ * @return If the function succeeds, the return value is nonzero. If the
+ *         function fails, the return value is zero. To get extended error
+ *         information, call GetLastError.
+ * @remarks Applications running at a DPI_AWARENESS_CONTEXT of
+ *          DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 automatically scale
+ *          their dialogs by default. They do not need to call this function.
+ *          Starting from Windows 10 Build 14986, Microsoft introduces the
+ *          Per-Monitor (V2) DPI Awareness.
+ *          Reference: https://blogs.windows.com/windows-insider/2016/12/07/
+ *                     announcing-windows-10-insider-preview-build-14986-pc
+ *          The Windows Notepad in Windows 10 Build 14986 started to use
+ *          Per-Monitor (V2) DPI Awareness.
+*/
+EXTERN_C BOOL WINAPI MileEnablePerMonitorDialogScaling();
+
+#endif // !MILE_WINDOWS_DPIHELPERS
