c2d22297656288b7d98f5b5786bb30a1eb78cb36
MouriNaruto <Mouri_Naruto@Outlook.com>
2022-07-09 20:15:30 +0800

    Continue tidy the implementation.

--- NanaZip.Shared/Mitigations.cpp
+++ NanaZip.Shared/Mitigations.cpp
@@ -13,47 +13,64 @@
 
 #include <VersionHelpers.h>
 
-typedef BOOL(WINAPI* Func_SetProcessMitigationPolicy)(
-    _In_ PROCESS_MITIGATION_POLICY MitigationPolicy,
-    _In_reads_bytes_(dwLength) PVOID lpBuffer,
-    _In_ SIZE_T dwLength);
-
 EXTERN_C BOOL WINAPI NanaZipEnableMitigations()
 {
     if (!IsWindows8OrGreater())
         return TRUE;
 
-    HMODULE mod = ::GetModuleHandleW(L"kernel32.dll");
-    if (!mod)
+    HMODULE ModuleHandle = ::GetModuleHandleW(L"kernel32.dll");
+    if (!ModuleHandle)
+    {
         return FALSE;
+    }
 
-    Func_SetProcessMitigationPolicy my_SetProcessMitigationPolicy =
-        (Func_SetProcessMitigationPolicy)GetProcAddress(mod, "SetProcessMitigationPolicy");
+    decltype(::SetProcessMitigationPolicy)* my_SetProcessMitigationPolicy =
+        reinterpret_cast<decltype(::SetProcessMitigationPolicy)*>(
+            ::GetProcAddress(ModuleHandle, "SetProcessMitigationPolicy"));
     if (!my_SetProcessMitigationPolicy)
+    {
         return FALSE;
+    }
 
-    PROCESS_MITIGATION_STRICT_HANDLE_CHECK_POLICY shcp = { 0 };
-    shcp.RaiseExceptionOnInvalidHandleReference = 1;
-    shcp.HandleExceptionsPermanentlyEnabled = 1;
-    if (!my_SetProcessMitigationPolicy(ProcessStrictHandleCheckPolicy, &shcp, sizeof(shcp)))
-        return FALSE;
+    {
+        PROCESS_MITIGATION_STRICT_HANDLE_CHECK_POLICY Policy = { 0 };
+        Policy.RaiseExceptionOnInvalidHandleReference = 1;
+        Policy.HandleExceptionsPermanentlyEnabled = 1;
+        if (!my_SetProcessMitigationPolicy(
+            ProcessStrictHandleCheckPolicy,
+            &Policy,
+            sizeof(PROCESS_MITIGATION_STRICT_HANDLE_CHECK_POLICY)))
+        {
+            return FALSE;
+        }
+    }
 
-    if (!IsWindows8Point1OrGreater())
-        return TRUE;
+    if (IsWindows8Point1OrGreater())
+    {
+        PROCESS_MITIGATION_DYNAMIC_CODE_POLICY Policy = { 0 };
+        Policy.ProhibitDynamicCode = 1;
+        if (!my_SetProcessMitigationPolicy(
+            ProcessDynamicCodePolicy,
+            &Policy,
+            sizeof(PROCESS_MITIGATION_DYNAMIC_CODE_POLICY)))
+        {
+            return FALSE;
+        }
+    }
 
-    PROCESS_MITIGATION_DYNAMIC_CODE_POLICY dcp = { 0 };
-    dcp.ProhibitDynamicCode = 1;
-    if (!my_SetProcessMitigationPolicy(ProcessDynamicCodePolicy, &dcp, sizeof(dcp)))
-        return FALSE;
-
-    if (!IsWindows10OrGreater())
-        return TRUE;
-
-    PROCESS_MITIGATION_IMAGE_LOAD_POLICY ilp = { 0 };
-    ilp.NoRemoteImages = 1;
-    ilp.NoLowMandatoryLabelImages = 1;
-    if (!my_SetProcessMitigationPolicy(ProcessImageLoadPolicy, &ilp, sizeof(ilp)))
-        return FALSE;
+    if (IsWindows10OrGreater())
+    {
+        PROCESS_MITIGATION_IMAGE_LOAD_POLICY Policy = { 0 };
+        Policy.NoRemoteImages = 1;
+        Policy.NoLowMandatoryLabelImages = 1;
+        if (!my_SetProcessMitigationPolicy(
+            ProcessImageLoadPolicy,
+            &Policy,
+            sizeof(PROCESS_MITIGATION_IMAGE_LOAD_POLICY)))
+        {
+            return FALSE;
+        }
+    }
 
     return TRUE;
 }
