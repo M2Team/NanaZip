e77e00ffa67473ad3b32cb75cf68c952533c6dee
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-01-10 02:06:46 +0800

    Update to Git submodule version of Mile.Project.Windows.

--- BuildAllTargets.cmd
+++ BuildAllTargets.cmd
@@ -1,13 +1,3 @@
-@rem 
-@rem PROJECT:   Mouri Internal Library Essentials
-@rem FILE:      BuildAllTargets.cmd
-@rem PURPOSE:   Build all targets script for Visual Studio C++ Project
-@rem 
-@rem LICENSE:   The MIT License
-@rem 
-@rem DEVELOPER: Mouri_Naruto (Mouri_Naruto AT Outlook.com)
-@rem 
-
 @setlocal
 @echo off
 
@@ -17,16 +7,8 @@ cd "%~dp0"
 rem Remove the output folder for a fresh compile.
 rd /s /q Output
 
-set VisualStudioInstallerFolder="%ProgramFiles(x86)%\Microsoft Visual Studio\Installer"
-if %PROCESSOR_ARCHITECTURE%==x86 set VisualStudioInstallerFolder="%ProgramFiles%\Microsoft Visual Studio\Installer"
-
-pushd %VisualStudioInstallerFolder%
-for /f "usebackq tokens=*" %%i in (`vswhere -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
-  set VisualStudioInstallDir=%%i
-)
-popd
-
-call "%VisualStudioInstallDir%\VC\Auxiliary\Build\vcvarsall.bat" x86
+rem Initialize Visual Studio environment
+call "%~dp0Mile.Project.Windows\InitializeVisualStudioEnvironment.cmd"
 
 rem Build all targets
 MSBuild BuildAllTargets.proj
