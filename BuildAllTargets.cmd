dd8c9711ea7633887c1cff9b52b0f30608075d68
MouriNaruto <Mouri_Naruto@Outlook.com>
2023-03-23 22:46:46 +0800

    Enable parallel compilation for BuildAllTargets.cmd script.

--- BuildAllTargets.cmd
+++ BuildAllTargets.cmd
@@ -11,6 +11,6 @@ rem Initialize Visual Studio environment
 call "%~dp0Mile.Project.Windows\InitializeVisualStudioEnvironment.cmd"
 
 rem Build all targets
-MSBuild -binaryLogger:Output\BuildAllTargets.binlog BuildAllTargets.proj
+MSBuild -binaryLogger:Output\BuildAllTargets.binlog -m BuildAllTargets.proj
 
 @endlocal
