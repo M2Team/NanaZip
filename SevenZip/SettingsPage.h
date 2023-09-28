7be24cb7020b6628e94046fac05acb6f34fbf49d
modz2014 <dawgz1992.jp@gmail.com>
2023-09-28 22:10:39 +0800

    Add files via upload
    Adding User Option to open folder destination after extraction is completed using shell extension

---
+++ SevenZip/SettingsPage.h
@@ -0,0 +1,35 @@
+﻿// SettingsPage.h
+
+#ifndef __SETTINGS_PAGE_H
+#define __SETTINGS_PAGE_H
+
+#include "../../../../../ThirdParty/LZMA/CPP/Windows/Control/PropertyPage.h"
+#include "../../../../../ThirdParty/LZMA/CPP/Windows/Control/ComboBox.h"
+#include "../../../../../ThirdParty/LZMA/CPP/Windows/Control/Edit.h"
+//#include "../../../../../NanaZipShellExtension/Config.h"
+class CSettingsPage: public NWindows::NControl::CPropertyPage
+{
+  bool _wasChanged;
+  bool _largePages_wasChanged;
+  
+  /*
+  bool _wasChanged_MemLimit;
+  NWindows::NControl::CComboBox _memCombo;
+  UStringVector _memLimitStrings;
+  UInt64 _ramSize;
+  UInt64 _ramSize_Defined;
+
+  int AddMemComboItem(UInt64 size, UInt64 percents = 0, bool isDefault = false);
+  */
+
+  // void EnableSubItems();
+  // bool OnCommand(int code, int itemID, LPARAM param);
+  bool OnButtonClicked(int buttonID, HWND buttonHWND);
+  virtual bool OnInit();
+  virtual LONG OnApply();
+public:
+   
+};
+
+
+#endif
