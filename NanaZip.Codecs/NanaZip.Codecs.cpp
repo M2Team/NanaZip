e0f282d8f513e6afee0067029a7ef8b09b158827
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-01 21:06:04 +0800

    Add NanaZip.Codecs project.

---
+++ NanaZip.Codecs/NanaZip.Codecs.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mile.Samples.DynamicLibrary
+ * FILE:      NanaZip.Codecs.cpp
+ * PURPOSE:   Implementation for NanaZip.Codecs
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+EXTERN_C int WINAPI NanaZipCodecsTestFunction()
+{
+    return 0;
+}
