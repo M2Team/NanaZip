936ed533c00f1b920921b232aa28fbbf5bb5cee1
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-01 19:43:24 +0800

    Add NanaZip.Specification.SevenZip.

---
+++ NanaZip.Specification/NanaZip.Specification.SevenZip.h
@@ -0,0 +1,14 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.Specification.SevenZip.h
+ * PURPOSE:   Definition for 7-Zip Codec Interface Specification
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANAZIP_SPECIFICATION_SEVENZIP
+#define NANAZIP_SPECIFICATION_SEVENZIP
+
+#endif /* !NANAZIP_SPECIFICATION_SEVENZIP */
