a6f2eb953efe8e6b914864f5903327a4eaef4f41
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-03 00:08:36 +0800

    Add 7-Zip Hasher interface definitions to NanaZip.Specification.SevenZip.

--- NanaZip.Specification/NanaZip.Specification.SevenZip.h
+++ NanaZip.Specification/NanaZip.Specification.SevenZip.h
@@ -11,4 +11,43 @@
 #ifndef NANAZIP_SPECIFICATION_SEVENZIP
 #define NANAZIP_SPECIFICATION_SEVENZIP
 
+#include <Windows.h>
+
+/* 7-Zip Interface GUID Format: 23170F69-40C1-278A-0000-00yy00xx0000 */
+
+MIDL_INTERFACE("23170F69-40C1-278A-0000-000400C00000")
+IHasher : public IUnknown
+{
+public:
+    virtual void STDMETHODCALLTYPE Init() = 0;
+
+    virtual void STDMETHODCALLTYPE Update(
+        _In_ LPCVOID Data,
+        _In_ UINT32 Size) = 0;
+
+    virtual void STDMETHODCALLTYPE Final(
+        _Out_ PBYTE Digest) = 0;
+
+    virtual UINT32 STDMETHODCALLTYPE GetDigestSize() = 0;
+};
+
+MIDL_INTERFACE("23170F69-40C1-278A-0000-000400C10000")
+IHashers: public IUnknown
+{
+public:
+    virtual UINT32 STDMETHODCALLTYPE GetNumHashers() = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE GetHasherProp(
+        _In_ UINT32 Index,
+        _In_ PROPID PropID,
+        _Out_ LPPROPVARIANT Value) = 0;
+
+    virtual HRESULT STDMETHODCALLTYPE CreateHasher(
+        _In_ UINT32 Index,
+        _Out_ IHasher** Hasher) = 0;
+};
+
+EXTERN_C HRESULT WINAPI GetHashers(
+    _Out_ IHashers** Hashers);
+
 #endif /* !NANAZIP_SPECIFICATION_SEVENZIP */
