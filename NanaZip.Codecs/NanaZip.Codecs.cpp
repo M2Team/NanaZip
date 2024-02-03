c4387e495f7c046692e3f3c215772dbe736bdf07
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-03 18:57:08 +0800

    Implement GetHashers and empty HasherFactory for NanaZip.Codecs.

--- NanaZip.Codecs/NanaZip.Codecs.cpp
+++ NanaZip.Codecs/NanaZip.Codecs.cpp
@@ -12,7 +12,55 @@
 
 #include <NanaZip.Specification.SevenZip.h>
 
-EXTERN_C int WINAPI NanaZipCodecsTestFunction()
+#include <winrt/Windows.Foundation.h>
+
+struct HasherFactory : public winrt::implements<
+    HasherFactory, IHashers>
 {
-    return 0;
+public:
+
+    UINT32 STDMETHODCALLTYPE GetNumHashers()
+    {
+        return 0;
+    }
+
+    HRESULT STDMETHODCALLTYPE GetHasherProp(
+        _In_ UINT32 Index,
+        _In_ PROPID PropID,
+        _Out_ LPPROPVARIANT Value)
+    {
+        UNREFERENCED_PARAMETER(Index);
+        UNREFERENCED_PARAMETER(PropID);
+        UNREFERENCED_PARAMETER(Value);
+        return E_NOTIMPL;
+    }
+
+    HRESULT STDMETHODCALLTYPE CreateHasher(
+        _In_ UINT32 Index,
+        _Out_ IHasher** Hasher)
+    {
+        UNREFERENCED_PARAMETER(Index);
+        UNREFERENCED_PARAMETER(Hasher);
+        return E_NOTIMPL;
+    }
+};
+
+EXTERN_C HRESULT WINAPI GetHashers(
+    _Out_ IHashers** Hashers)
+{
+    if (!Hashers)
+    {
+        return E_INVALIDARG;
+    }
+
+    try
+    {
+        *Hashers = winrt::make<HasherFactory>().detach();
+    }
+    catch (...)
+    {
+        return winrt::to_hresult();
+    }
+
+    return S_OK;
 }
