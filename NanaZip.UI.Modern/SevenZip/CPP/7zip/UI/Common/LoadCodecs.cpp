62278f2180d26f1efb79f76d5e1cd49acaac59c4
MouriNaruto <Kenji.Mouri@outlook.com>
2024-02-09 13:57:52 +0800

    Temporary workaround for developing NanaZip.Codecs.dll for solving issue mentioned in https://github.com/M2Team/NanaZip/pull/379. (Thanks to dinhngtu.)

--- NanaZip.UI.Modern/SevenZip/CPP/7zip/UI/Common/LoadCodecs.cpp
+++ NanaZip.UI.Modern/SevenZip/CPP/7zip/UI/Common/LoadCodecs.cpp
@@ -1,4 +1,4 @@
-// LoadCodecs.cpp
+﻿// LoadCodecs.cpp
 
 /*
 EXTERNAL_CODECS
@@ -781,6 +781,11 @@ HRESULT CCodecs::Load()
   #ifdef EXTERNAL_CODECS
     const FString baseFolder = GetBaseFolderPrefixFromRegistry();
     {
+      // ******** NanaZip.Codecs.dll development workaround start ********
+      // For workaround https://github.com/M2Team/NanaZip/pull/379
+      if (!::LoadLibraryW(baseFolder + L"NanaZip.Codecs.dll"))
+          MainDll_ErrorPath = L"NanaZip.Codecs.dll";
+      // ******** NanaZip.Codecs.dll development workaround end ********
       bool loadedOK;
       RINOK(LoadDll(baseFolder + kMainDll, false, &loadedOK));
       if (!loadedOK)
