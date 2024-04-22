6ee2a496bcc65f7e208937b8a83954ba599367c1
MouriNaruto <Kenji.Mouri@outlook.com>
2024-04-22 21:19:20 +0800

    Try to add the workaround for unhandled exception at twinapi.appcore.dll for fixing https://github.com/M2Team/NanaZip/issues/400.

--- NanaZip.UI.Modern/App.xaml.cpp
+++ NanaZip.UI.Modern/App.xaml.cpp
@@ -8,6 +8,9 @@ namespace winrt::NanaZip::Modern::implementation
 {
     App::App()
     {
+        // Workaround for unhandled exception at twinapi.appcore.dll
+        // Fixes: https://github.com/M2Team/NanaZip/issues/400
+        this->AddRef();
         ::MileXamlGlobalInitialize();
     }
 
