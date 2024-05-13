23bca2ec275b772f416b027b22ee9eadcf0337c0
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-13 20:46:42 +0800

    Create NanaZip.Frieren.

---
+++ NanaZip.Frieren/NanaZip.Frieren.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.Frieren.cpp
+ * PURPOSE:   Implementation for NanaZip.Frieren
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "NanaZip.Frieren.h"
---
+++ NanaZip.Frieren/NanaZip.Frieren.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.Frieren.h
+ * PURPOSE:   Definition for NanaZip.Frieren
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANAZIP_FRIEREN
+#define NANAZIP_FRIEREN
+
+#include <Windows.h>
+
+#endif // !NANAZIP_FRIEREN
