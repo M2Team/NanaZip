0671a39db20b8a1dd49bc8ee9067c3cbeb836e8f
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-18 01:04:16 +0800

    Add RoGetActivationFactory to NanaZip.Frieren.RuntimeObjectWrapper.

--- NanaZip.Frieren/NanaZip.Frieren.RuntimeObjectWrapper.cpp
+++ NanaZip.Frieren/NanaZip.Frieren.RuntimeObjectWrapper.cpp
@@ -9,3 +9,54 @@
  */
 
 #include <Windows.h>
+
+#define _ROAPI_
+#include <roapi.h>
+
+namespace
+{
+    static HMODULE GetComBaseModuleHandle()
+    {
+        static HMODULE CachedResult = ::LoadLibraryExW(
+            L"combase.dll",
+            nullptr,
+            LOAD_LIBRARY_SEARCH_SYSTEM32);
+        return CachedResult;
+    }
+
+    static FARPROC GetRoGetActivationFactoryProcAddress()
+    {
+        static FARPROC CachedResult = ([]() -> FARPROC
+        {
+            HMODULE ModuleHandle = ::GetComBaseModuleHandle();
+            if (ModuleHandle)
+            {
+                return ::GetProcAddress(
+                    ModuleHandle,
+                    "RoGetActivationFactory");
+            }
+            return nullptr;
+        }());
+
+        return CachedResult;
+    }
+}
+
+EXTERN_C HRESULT WINAPI RoGetActivationFactory(
+    _In_ HSTRING activatableClassId,
+    _In_ REFIID iid,
+    _Out_ LPVOID* factory)
+{
+    using ProcType = decltype(::RoGetActivationFactory)*;
+
+    ProcType ProcAddress = reinterpret_cast<ProcType>(
+        ::GetRoGetActivationFactoryProcAddress());
+
+    if (ProcAddress)
+    {
+        return ProcAddress(activatableClassId, iid, factory);
+    }
+
+    *factory = nullptr;
+    return CLASS_E_CLASSNOTAVAILABLE;
+}
