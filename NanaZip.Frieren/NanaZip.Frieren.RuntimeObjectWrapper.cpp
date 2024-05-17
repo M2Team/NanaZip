1878af0d9314d2d13872f1c8150d1c3e7a0b06e3
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-18 00:59:33 +0800

    Add NanaZip.Frieren.RuntimeObjectWrapper.

---
+++ NanaZip.Frieren/NanaZip.Frieren.RuntimeObjectWrapper.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.Frieren.RuntimeObjectWrapper.cpp
+ * PURPOSE:   Implementation for Windows Runtime C API Wrapper
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
