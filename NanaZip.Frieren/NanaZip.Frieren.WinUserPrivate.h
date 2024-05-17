560adc1411d8585e87145273ee4a35f919352b62
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-17 21:23:05 +0800

    Split Windows undocumented window messages to NanaZip.Frieren.WinUserPrivate.

---
+++ NanaZip.Frieren/NanaZip.Frieren.WinUserPrivate.h
@@ -0,0 +1,88 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.Frieren.WinUserPrivate.h
+ * PURPOSE:   Definition for Windows undocumented window messages
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANAZIP_FRIEREN_WINUSERPRIVATE
+#define NANAZIP_FRIEREN_WINUSERPRIVATE
+
+#include <Windows.h>
+
+// Reference: https://github.com/adzm/win32-custom-menubar-aero-theme
+
+// window messages related to menu bar drawing
+#define WM_UAHDESTROYWINDOW    0x0090	// handled by DefWindowProc
+#define WM_UAHDRAWMENU         0x0091	// lParam is UAHMENU
+#define WM_UAHDRAWMENUITEM     0x0092	// lParam is UAHDRAWMENUITEM
+#define WM_UAHINITMENU         0x0093	// handled by DefWindowProc
+#define WM_UAHMEASUREMENUITEM  0x0094	// lParam is UAHMEASUREMENUITEM
+#define WM_UAHNCPAINTMENUPOPUP 0x0095	// handled by DefWindowProc
+
+// describes the sizes of the menu bar or menu item
+typedef union tagUAHMENUITEMMETRICS
+{
+    // cx appears to be 14 / 0xE less than rcItem's width!
+    // cy 0x14 seems stable, i wonder if it is 4 less than rcItem's height which
+    //    is always 24 atm
+    struct {
+        DWORD cx;
+        DWORD cy;
+    } rgsizeBar[2];
+    struct {
+        DWORD cx;
+        DWORD cy;
+    } rgsizePopup[4];
+} UAHMENUITEMMETRICS, *PUAHMENUITEMMETRICS;
+
+// not really used in our case but part of the other structures
+typedef struct tagUAHMENUPOPUPMETRICS
+{
+    DWORD rgcx[4];
+
+    // from kernel symbols, padded to full dword
+    DWORD fUpdateMaxWidths : 2;
+} UAHMENUPOPUPMETRICS, *PUAHMENUPOPUPMETRICS;
+
+// hmenu is the main window menu; hdc is the context to draw in
+typedef struct tagUAHMENU
+{
+    HMENU hmenu;
+    HDC hdc;
+    // no idea what these mean, in my testing it's either 0x00000a00 or
+    // sometimes 0x00000a10
+    DWORD dwFlags;
+} UAHMENU, *PUAHMENU;
+
+// menu items are always referred to by iPosition here
+typedef struct tagUAHMENUITEM
+{
+    int iPosition; // 0-based position of menu item in menubar
+    UAHMENUITEMMETRICS umim;
+    UAHMENUPOPUPMETRICS umpm;
+} UAHMENUITEM, *PUAHMENUITEM;
+
+// the DRAWITEMSTRUCT contains the states of the menu items, as well as
+// the position index of the item in the menu, which is duplicated in
+// the UAHMENUITEM's iPosition as well
+typedef struct UAHDRAWMENUITEM
+{
+    DRAWITEMSTRUCT dis; // itemID looks uninitialized
+    UAHMENU um;
+    UAHMENUITEM umi;
+} UAHDRAWMENUITEM, *PUAHDRAWMENUITEM;
+
+// the MEASUREITEMSTRUCT is intended to be filled with the size of the item
+// height appears to be ignored, but width can be modified
+typedef struct tagUAHMEASUREMENUITEM
+{
+    MEASUREITEMSTRUCT mis;
+    UAHMENU um;
+    UAHMENUITEM umi;
+} UAHMEASUREMENUITEM, *PUAHMEASUREMENUITEM;
+
+#endif // !NANAZIP_FRIEREN_WINUSERPRIVATE
--- NanaZip.Frieren/NanaZip.Frieren.cpp
+++ NanaZip.Frieren/NanaZip.Frieren.cpp
@@ -21,77 +21,7 @@
 
 #include <unordered_map>
 
-// Reference: https://github.com/adzm/win32-custom-menubar-aero-theme
-
-// window messages related to menu bar drawing
-#define WM_UAHDESTROYWINDOW    0x0090	// handled by DefWindowProc
-#define WM_UAHDRAWMENU         0x0091	// lParam is UAHMENU
-#define WM_UAHDRAWMENUITEM     0x0092	// lParam is UAHDRAWMENUITEM
-#define WM_UAHINITMENU         0x0093	// handled by DefWindowProc
-#define WM_UAHMEASUREMENUITEM  0x0094	// lParam is UAHMEASUREMENUITEM
-#define WM_UAHNCPAINTMENUPOPUP 0x0095	// handled by DefWindowProc
-
-// describes the sizes of the menu bar or menu item
-typedef union tagUAHMENUITEMMETRICS
-{
-    // cx appears to be 14 / 0xE less than rcItem's width!
-    // cy 0x14 seems stable, i wonder if it is 4 less than rcItem's height which
-    //    is always 24 atm
-    struct {
-        DWORD cx;
-        DWORD cy;
-    } rgsizeBar[2];
-    struct {
-        DWORD cx;
-        DWORD cy;
-    } rgsizePopup[4];
-} UAHMENUITEMMETRICS;
-
-// not really used in our case but part of the other structures
-typedef struct tagUAHMENUPOPUPMETRICS
-{
-    DWORD rgcx[4];
-
-    // from kernel symbols, padded to full dword
-    DWORD fUpdateMaxWidths : 2;
-} UAHMENUPOPUPMETRICS;
-
-// hmenu is the main window menu; hdc is the context to draw in
-typedef struct tagUAHMENU
-{
-    HMENU hmenu;
-    HDC hdc;
-    // no idea what these mean, in my testing it's either 0x00000a00 or
-    // sometimes 0x00000a10
-    DWORD dwFlags;
-} UAHMENU;
-
-// menu items are always referred to by iPosition here
-typedef struct tagUAHMENUITEM
-{
-    int iPosition; // 0-based position of menu item in menubar
-    UAHMENUITEMMETRICS umim;
-    UAHMENUPOPUPMETRICS umpm;
-} UAHMENUITEM;
-
-// the DRAWITEMSTRUCT contains the states of the menu items, as well as
-// the position index of the item in the menu, which is duplicated in
-// the UAHMENUITEM's iPosition as well
-typedef struct UAHDRAWMENUITEM
-{
-    DRAWITEMSTRUCT dis; // itemID looks uninitialized
-    UAHMENU um;
-    UAHMENUITEM umi;
-} UAHDRAWMENUITEM;
-
-// the MEASUREITEMSTRUCT is intended to be filled with the size of the item
-// height appears to be ignored, but width can be modified
-typedef struct tagUAHMEASUREMENUITEM
-{
-    MEASUREITEMSTRUCT mis;
-    UAHMENU um;
-    UAHMENUITEM umi;
-} UAHMEASUREMENUITEM;
+#include "NanaZip.Frieren.WinUserPrivate.h"
 
 namespace
 {
@@ -360,7 +290,7 @@ namespace
         {
             if (WM_UAHDRAWMENU == uMsg)
             {
-                UAHMENU* UahMenu = reinterpret_cast<UAHMENU*>(lParam);
+                PUAHMENU UahMenu = reinterpret_cast<PUAHMENU>(lParam);
                 if (UahMenu)
                 {
                     MENUBARINFO MenuBarInfo;
@@ -387,8 +317,8 @@ namespace
             }
             else if (WM_UAHDRAWMENUITEM == uMsg)
             {
-                UAHDRAWMENUITEM* UahDrawMenuItem =
-                    reinterpret_cast<UAHDRAWMENUITEM*>(lParam);
+                PUAHDRAWMENUITEM UahDrawMenuItem =
+                    reinterpret_cast<PUAHDRAWMENUITEM>(lParam);
                 if (UahDrawMenuItem)
                 {
                     PDRAWITEMSTRUCT DrawItemStruct = &UahDrawMenuItem->dis;
