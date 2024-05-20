ddacad0e3b0cd52e6640f563789eb7175a864ea2
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-21 00:00:51 +0800

    Add NanaZip::UI::CreateXamlDialog, NanaZip::UI::ShowXamlWindow and NanaZip::UI::ShowXamlDialog.

--- NanaZip.UI.Modern/NanaZip.UI.h
+++ NanaZip.UI.Modern/NanaZip.UI.h
@@ -13,4 +13,24 @@
 
 #include <Windows.h>
 
+namespace NanaZip::UI
+{
+    HWND CreateXamlDialog(
+        _In_opt_ HWND ParentWindowHandle);
+
+    int ShowXamlWindow(
+        _In_opt_ HWND WindowHandle,
+        _In_ int Width,
+        _In_ int Height,
+        _In_ LPVOID Content,
+        _In_ HWND ParentWindowHandle);
+
+    int ShowXamlDialog(
+        _In_opt_ HWND WindowHandle,
+        _In_ int Width,
+        _In_ int Height,
+        _In_ LPVOID Content,
+        _In_ HWND ParentWindowHandle);
+}
+
 #endif // !NANAZIP_UI
