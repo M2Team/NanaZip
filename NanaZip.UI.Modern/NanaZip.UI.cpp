8c7fba28713661bc0fda489555e4570bd30d07f4
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-20 23:46:44 +0800

    Add NanaZip.UI.

---
+++ NanaZip.UI.Modern/NanaZip.UI.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.UI.cpp
+ * PURPOSE:   Implementation for NanaZip Modern UI Shared Infrastructures
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "NanaZip.UI.h"
---
+++ NanaZip.UI.Modern/NanaZip.UI.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.UI.h
+ * PURPOSE:   Definition for NanaZip Modern UI Shared Infrastructures
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANAZIP_UI
+#define NANAZIP_UI
+
+#include <Windows.h>
+
+#endif // !NANAZIP_UI
