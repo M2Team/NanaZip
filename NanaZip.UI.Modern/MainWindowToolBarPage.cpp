126a3fcf05d7eed387a1acbdcd8025d6fe8a9b26
MouriNaruto <Kenji.Mouri@outlook.com>
2024-05-21 00:52:12 +0800

    Add XAML page for new toolbar in the main window.

---
+++ NanaZip.UI.Modern/MainWindowToolBarPage.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "MainWindowToolBarPage.h"
+#if __has_include("MainWindowToolBarPage.g.cpp")
+#include "MainWindowToolBarPage.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::NanaZip::Modern::implementation
+{
+    MainWindowToolBarPage::MainWindowToolBarPage(
+        _In_ HWND WindowHandle) :
+        m_WindowHandle(WindowHandle)
+    {
+
+    }
+
+    void MainWindowToolBarPage::InitializeComponent()
+    {
+        MainWindowToolBarPageT::InitializeComponent();
+    }
+}
---
+++ NanaZip.UI.Modern/MainWindowToolBarPage.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "MainWindowToolBarPage.g.h"
+
+#include <Windows.h>
+
+namespace winrt
+{
+    using Windows::Foundation::IInspectable;
+    using Windows::UI::Xaml::RoutedEventArgs;
+}
+
+namespace winrt::NanaZip::Modern::implementation
+{
+    struct MainWindowToolBarPage : MainWindowToolBarPageT<MainWindowToolBarPage>
+    {
+    public:
+
+        MainWindowToolBarPage(
+            _In_ HWND WindowHandle = nullptr);
+
+        void InitializeComponent();
+
+    private:
+
+        HWND m_WindowHandle;
+    };
+}
+
+namespace winrt::NanaZip::Modern::factory_implementation
+{
+    struct MainWindowToolBarPage : MainWindowToolBarPageT<
+        MainWindowToolBarPage, implementation::MainWindowToolBarPage>
+    {
+    };
+}
