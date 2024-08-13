dcbbec1b4cc94572b1a6ff1e5c94aaeabf2b6221
MouriNaruto <Kenji.Mouri@outlook.com>
2024-08-13 23:57:47 +0800

    Create NanaZip.RefreshPackageVersion project.

---
+++ NanaZip.RefreshPackageVersion/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace NanaZip.RefreshPackageVersion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(
+               "NanaZip.RefreshPackageVersion task has been completed.");
+            Console.ReadKey();
+        }
+    }
+}
