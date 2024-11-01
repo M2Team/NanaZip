845d94c2d6b72b02da7bf783fa0e241b3da4e21a
MouriNaruto <Kenji.Mouri@outlook.com>
2024-11-02 00:26:26 +0800

    Create NanaZip.Codecs.Wrappers.ZSTDMT.

---
+++ NanaZip.Codecs/NanaZip.Codecs.Wrappers.ZSTDMT.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.Codecs.Wrappers.ZSTDMT.cpp
+ * PURPOSE:   Implementation for NanaZip.Codecs ZSTDMT Wrappers
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "NanaZip.Codecs.Wrappers.ZSTDMT.h"
---
+++ NanaZip.Codecs/NanaZip.Codecs.Wrappers.ZSTDMT.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.Codecs.Wrappers.ZSTDMT.h
+ * PURPOSE:   Definition for NanaZip.Codecs ZSTDMT Wrappers
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANAZIP_CODECS_WRAPPERS_ZSTDMT
+#define NANAZIP_CODECS_WRAPPERS_ZSTDMT
+
+#include <NanaZip.Specification.SevenZip.h>
+
+#endif // !NANAZIP_CODECS_WRAPPERS_ZSTDMT
