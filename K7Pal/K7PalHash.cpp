54170cfb306b297f57638aac526b25dffc71e604
MouriNaruto <Kenji.Mouri@outlook.com>
2024-12-06 00:23:32 +0800

    Add K7PalHash.cpp.

---
+++ K7Pal/K7PalHash.cpp
@@ -0,0 +1,11 @@
+﻿/*
+ * PROJECT:   NanaZip Platform Abstraction Layer (K7Pal)
+ * FILE:      K7PalHash.cpp
+ * PURPOSE:   Implementation for K7Pal Hash Algorithms Interfaces
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "K7Pal.h"
