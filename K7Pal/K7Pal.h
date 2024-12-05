34b17b2f21a71bbc5bb083b51442b9b20fff35b1
MouriNaruto <Kenji.Mouri@outlook.com>
2024-12-06 00:20:43 +0800

    Add K7Pal.h.

---
+++ K7Pal/K7Pal.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   NanaZip Platform Abstraction Layer (K7Pal)
+ * FILE:      K7Pal.h
+ * PURPOSE:   Definition for NanaZip Platform Abstraction Layer (K7Pal)
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef K7_PAL
+#define K7_PAL
+
+#include <Windows.h>
+
+#endif // !K7_PAL
