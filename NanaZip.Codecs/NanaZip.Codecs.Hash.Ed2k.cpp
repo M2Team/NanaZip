87108f9ea9ccfd1ee54ea09558a760cf8cf88318
MouriNaruto <Kenji.Mouri@outlook.com>
2024-12-19 00:21:21 +0800

    Use K7Pal instead of RHash to implement the ED2K hash algorithm.

--- NanaZip.Codecs/NanaZip.Codecs.Hash.Ed2k.cpp
+++ NanaZip.Codecs/NanaZip.Codecs.Hash.Ed2k.cpp
@@ -10,7 +10,27 @@
 
 #include "NanaZip.Codecs.h"
 
-#include <ed2k.h>
+#include <K7Pal.h>
+
+// Comments copy from RHash's ed2k.c
+//
+// This file implements eMule-compatible version of algorithm. Note that
+// eDonkey and eMule ed2k hashes are different only for files containing
+// exactly multiple of 9728000 bytes.
+// The file data is divided into full chunks of 9500 KiB (9728000 bytes) plus a
+// remainder chunk, and a separate 128-bit MD4 hash is computed for each. If the
+// file length is an exact multiple of 9500 KiB, the remainder zero size chunk
+// is still used at the end of the hash list. The ed2k hash is computed by
+// concatenating the chunks' MD4 hashes in order and hashing the result using
+// MD4. Although, if the file is composed of a single non-full chunk, its MD4
+// hash is returned with no further modifications.
+// See http://en.wikipedia.org/wiki/EDonkey_network for algorithm description.
+
+// Each hashed file is divided into 9500 KiB sized chunks
+#define ED2K_CHUNK_SIZE 9728000
+
+#define MD4_HASH_SIZE 16
+#define ED2K_HASH_SIZE MD4_HASH_SIZE
 
 namespace NanaZip::Codecs::Hash
 {
@@ -18,42 +38,150 @@ namespace NanaZip::Codecs::Hash
     {
     private:
 
-        ed2k_ctx Context;
+        // MD4 context to hash file blocks.
+        K7_PAL_HASH_HANDLE m_BlockHashHandle = nullptr;
+
+        // MD4 context to hash block hashes.
+        K7_PAL_HASH_HANDLE m_HashesHashHandle = nullptr;
+
+        // false for eMule ED2K algorithm.
+        bool m_NotEmule = false;
+
+        // The processed size for hash file blocks.
+        UINT32 m_BlockProcessedSize = 0;
+
+        // The processed size for hash block hashes.
+        UINT32 m_HashesProcessedSize = 0;
 
     public:
 
         Ed2k()
         {
-            this->Init();
+            ::K7PalHashCreate(
+                &this->m_BlockHashHandle,
+                BCRYPT_MD4_ALGORITHM,
+                nullptr,
+                0);
+            ::K7PalHashCreate(
+                &this->m_HashesHashHandle,
+                BCRYPT_MD4_ALGORITHM,
+                nullptr,
+                0);
+            this->m_NotEmule = false;
+            this->m_BlockProcessedSize = 0;
         }
 
         void STDMETHODCALLTYPE Init()
         {
-            ::rhash_ed2k_init(
-                &this->Context);
+            ::K7PalHashReset(this->m_BlockHashHandle);
+            ::K7PalHashReset(this->m_HashesHashHandle);
+            this->m_NotEmule = false;
+            this->m_BlockProcessedSize = 0;
         }
 
         void STDMETHODCALLTYPE Update(
             _In_ LPCVOID Data,
             _In_ UINT32 Size)
         {
-            ::rhash_ed2k_update(
-                &this->Context,
-                reinterpret_cast<const unsigned char*>(Data),
-                Size);
+            BYTE BlockHash[MD4_HASH_SIZE];
+            UINT32 BlockLeft = ED2K_CHUNK_SIZE - this->m_BlockProcessedSize;
+
+            // Note: eMule-compatible algorithm hashes by md4_inner the messages
+            // which sizes are multiple of 9728000 and then processes obtained
+            // hash by external MD4.
+
+            while (Size >= BlockLeft)
+            {
+                if (Size == BlockLeft && this->m_NotEmule)
+                {
+                    break;
+                }
+
+                // If internal ED2K chunk is full, then finalize it.
+                ::K7PalHashUpdate(
+                    this->m_BlockHashHandle,
+                    const_cast<LPVOID>(Data),
+                    BlockLeft);
+                this->m_BlockProcessedSize += BlockLeft;
+                Data = reinterpret_cast<LPCVOID>(
+                    reinterpret_cast<UINT_PTR>(Data) + BlockLeft);
+                Size -= BlockLeft;
+                BlockLeft = ED2K_CHUNK_SIZE;
+
+                // Just finished an ED2K chunk, updating MD4 external context.
+                ::K7PalHashFinal(
+                    this->m_BlockHashHandle,
+                    BlockHash,
+                    MD4_HASH_SIZE);
+                this->m_BlockProcessedSize = 0;
+                ::K7PalHashUpdate(
+                    this->m_HashesHashHandle,
+                    BlockHash,
+                    MD4_HASH_SIZE);
+                this->m_HashesProcessedSize += MD4_HASH_SIZE;
+            }
+
+            if (Size)
+            {
+                // Hash leftovers.
+                ::K7PalHashUpdate(
+                    this->m_BlockHashHandle,
+                    const_cast<LPVOID>(Data),
+                    Size);
+                this->m_BlockProcessedSize += Size;
+            }
         }
 
         void STDMETHODCALLTYPE Final(
             _Out_ PBYTE Digest)
         {
-            ::rhash_ed2k_final(
-                &this->Context,
-                Digest);
+            // Check if hashed message size is greater or equal to
+            // ED2K_CHUNK_SIZE.
+            if (this->m_HashesProcessedSize)
+            {
+                // Note: Weird eMule algorithm always processes the inner MD4
+                // context, no matter if it contains data or is empty.
+
+                // If any data are left in the m_BlockHashHandle.
+                if (this->m_BlockProcessedSize || !this->m_NotEmule)
+                {
+                    // eMule algorithm processes aditional block, even if it's
+                    // empty.
+
+                    BYTE BlockHash[MD4_HASH_SIZE];
+                    ::K7PalHashFinal(
+                        this->m_BlockHashHandle,
+                        BlockHash,
+                        MD4_HASH_SIZE);
+                    this->m_BlockProcessedSize = 0;
+                    ::K7PalHashUpdate(
+                        this->m_HashesHashHandle,
+                        BlockHash,
+                        MD4_HASH_SIZE);
+                    this->m_HashesProcessedSize += MD4_HASH_SIZE;
+                }
+
+                // Call final to flush MD4 buffer and finalize the hash value.
+                ::K7PalHashFinal(
+                    this->m_HashesHashHandle,
+                    Digest,
+                    ED2K_HASH_SIZE);
+                this->m_HashesProcessedSize = 0;
+            }
+            else
+            {
+                // Just return the message MD4 hash.
+                ::K7PalHashFinal(
+                    this->m_BlockHashHandle,
+                    Digest,
+                    MD4_HASH_SIZE);
+                this->m_BlockProcessedSize = 0;
+            }
         }
 
         UINT32 STDMETHODCALLTYPE GetDigestSize()
         {
-            return 16;
+            return ED2K_HASH_SIZE;
         }
     };
 
