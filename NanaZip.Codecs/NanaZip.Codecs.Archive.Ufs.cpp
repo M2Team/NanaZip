4bd2f08b104f945936fdd8a128a266c02892966e
MouriNaruto <Kenji.Mouri@outlook.com>
2025-01-29 22:47:07 +0800

    Continue to implement the UFS/UFS2 file system image readonly support for NanaZip.Codecs.

--- NanaZip.Codecs/NanaZip.Codecs.Archive.Ufs.cpp
+++ NanaZip.Codecs/NanaZip.Codecs.Archive.Ufs.cpp
@@ -33,8 +33,114 @@
 
 namespace NanaZip::Codecs::Archive
 {
+    struct Ufs : public Mile::ComObject<Ufs, IInArchive>
+    {
+    public:
+
+        Ufs()
+        {
+
+        }
+
+        HRESULT STDMETHODCALLTYPE Open(
+            _In_ IInStream* Stream,
+            _In_opt_ const PUINT64 MaxCheckStartPosition,
+            _In_opt_ IArchiveOpenCallback* OpenCallback)
+        {
+            UNREFERENCED_PARAMETER(Stream);
+            UNREFERENCED_PARAMETER(MaxCheckStartPosition);
+            UNREFERENCED_PARAMETER(OpenCallback);
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE Close()
+        {
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetNumberOfItems(
+            _Out_ PUINT32 NumItems)
+        {
+            UNREFERENCED_PARAMETER(NumItems);
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetProperty(
+            _In_ UINT32 Index,
+            _In_ PROPID PropID,
+            _Inout_ LPPROPVARIANT Value)
+        {
+            UNREFERENCED_PARAMETER(Index);
+            UNREFERENCED_PARAMETER(PropID);
+            UNREFERENCED_PARAMETER(Value);
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE Extract(
+            _In_ const PUINT32 Indices,
+            _In_ UINT32 NumItems,
+            _In_ BOOL TestMode,
+            _In_ IArchiveExtractCallback* ExtractCallback)
+        {
+            UNREFERENCED_PARAMETER(Indices);
+            UNREFERENCED_PARAMETER(NumItems);
+            UNREFERENCED_PARAMETER(TestMode);
+            UNREFERENCED_PARAMETER(ExtractCallback);
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetArchiveProperty(
+            _In_ PROPID PropID,
+            _Inout_ LPPROPVARIANT Value)
+        {
+            UNREFERENCED_PARAMETER(PropID);
+            UNREFERENCED_PARAMETER(Value);
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetNumberOfProperties(
+            _Out_ PUINT32 NumProps)
+        {
+            UNREFERENCED_PARAMETER(NumProps);
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetPropertyInfo(
+            _In_ UINT32 Index,
+            _Out_ BSTR* Name,
+            _Out_ PROPID* PropID,
+            _Out_ VARTYPE* VarType)
+        {
+            UNREFERENCED_PARAMETER(Index);
+            UNREFERENCED_PARAMETER(Name);
+            UNREFERENCED_PARAMETER(PropID);
+            UNREFERENCED_PARAMETER(VarType);
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetNumberOfArchiveProperties(
+            _Out_ PUINT32 NumProps)
+        {
+            UNREFERENCED_PARAMETER(NumProps);
+            return E_NOTIMPL;
+        }
+
+        HRESULT STDMETHODCALLTYPE GetArchivePropertyInfo(
+            _In_ UINT32 Index,
+            _Out_ BSTR* Name,
+            _Out_ PROPID* PropID,
+            _Out_ VARTYPE* VarType)
+        {
+            UNREFERENCED_PARAMETER(Index);
+            UNREFERENCED_PARAMETER(Name);
+            UNREFERENCED_PARAMETER(PropID);
+            UNREFERENCED_PARAMETER(VarType);
+            return E_NOTIMPL;
+        }
+    };
+
     IInArchive* CreateUfs()
     {
-        return nullptr;
+        return new Ufs();
     }
 }
