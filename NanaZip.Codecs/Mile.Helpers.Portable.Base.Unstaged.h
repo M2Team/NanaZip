920920edd451921a1c317526d47d3cfe78dbabfc
MouriNaruto <Kenji.Mouri@outlook.com>
2025-03-09 01:16:52 +0800

    Introduce Mile.Helpers.Portable.Base.Unstaged as the temporary infrastructure to standardize some portable things in NanaZip.Codecs.

---
+++ NanaZip.Codecs/Mile.Helpers.Portable.Base.Unstaged.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   Mouri Internal Library Essentials
+ * FILE:      Mile.Helpers.Portable.Base.Unstaged.h
+ * PURPOSE:   Definition for unstaged portable essential helper functions
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef MILE_PORTABLE_HELPERS_BASE_UNSTAGED
+#define MILE_PORTABLE_HELPERS_BASE_UNSTAGED
+
+#include <stdint.h>
+
+#endif // !MILE_PORTABLE_HELPERS_BASE_UNSTAGED
