87db884e9df804748525f6ae7f7d77a5c07534f3
MouriNaruto <Kenji.Mouri@outlook.com>
2025-03-15 16:12:35 +0800

    Create NanaZip.Codecs.Specification.Fat.

---
+++ NanaZip.Codecs/NanaZip.Codecs.Specification.Fat.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      NanaZip.Codecs.Specification.Fat.h
+ * PURPOSE:   Definition for the on-disk structure of the FAT series file system
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef NANAZIP_CODECS_SPECIFICATION_FAT
+#define NANAZIP_CODECS_SPECIFICATION_FAT
+
+#include <Windows.h>
+
+#endif // !NANAZIP_CODECS_SPECIFICATION_FAT
