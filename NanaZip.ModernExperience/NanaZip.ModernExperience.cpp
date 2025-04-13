7270c3726ad20590a41fc65c250cdfb8ad1eef32
MouriNaruto <Kenji.Mouri@outlook.com>
2025-04-14 00:42:05 +0800

    Create NanaZip.ModernExperience project.

---
+++ NanaZip.ModernExperience/NanaZip.ModernExperience.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   NanaZip.ModernExperience
+ * FILE:      NanaZip.ModernExperience.cpp
+ * PURPOSE:   Implementation for NanaZip Modern Experience
+ *
+ * LICENSE:   The MIT License
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include <Windows.h>
+
+EXTERN_C int WINAPI NanaZipModernExperienceTestFunction()
+{
+    return 0;
+}
