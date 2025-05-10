0d0136f4227c0d9af45fcb95d7243f1738b9d62e
MouriNaruto <Kenji.Mouri@outlook.com>
2025-05-10 12:17:23 +0800

    Add App.xaml implementation to NanaZip.ModernExperience.

---
+++ NanaZip.ModernExperience/App.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::NanaZip::ModernExperience::implementation
+{
+    class App : public AppT<App>
+    {
+    public:
+        App();
+        void Close();
+    };
+}
---
+++ NanaZip.ModernExperience/App.xaml.cpp
@@ -0,0 +1,22 @@
+﻿#include "pch.h"
+
+#include "App.h"
+
+#include <Mile.Xaml.h>
+
+namespace winrt::NanaZip::ModernExperience::implementation
+{
+    App::App()
+    {
+        // Workaround for unhandled exception at twinapi.appcore.dll
+        // Fixes: https://github.com/M2Team/NanaZip/issues/400
+        this->AddRef();
+        ::MileXamlGlobalInitialize();
+    }
+
+    void App::Close()
+    {
+        Exit();
+        ::MileXamlGlobalUninitialize();
+    }
+}
