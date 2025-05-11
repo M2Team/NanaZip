9948e3b10e5b1f9bf5f020a054c6c3086a793c89
Tu Dinh <contact@tudinh.xyz>
2025-05-11 09:05:57 +0200

    Add NanaZip.Extension copy hook extension project.
    Signed-off-by: Tu Dinh <contact@tudinh.xyz>

---
+++ NanaZip.ExtensionPackage/CNanaZipCopyHook.hpp
@@ -0,0 +1,32 @@
+﻿/*
+ * PROJECT:   NanaZip
+ * FILE:      CNanaZipCopyHook.cpp
+ * PURPOSE:   Copy hook implementation
+ *
+ * LICENSE:   The MIT License
+ *
+ * DEVELOPER: dinhngtu (contact@tudinh.xyz)
+ */
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <windows.h>
+#include <winrt/windows.foundation.h>
+#include <ShlObj.h>
+
+struct CNanaZipCopyHook : winrt::implements<CNanaZipCopyHook, ICopyHookW> {
+    STDMETHOD_(UINT, CopyCallback)(
+        _In_opt_ HWND hwnd,
+        UINT wFunc,
+        UINT wFlags,
+        _In_ PCWSTR pszSrcFile,
+        DWORD dwSrcAttribs,
+        _In_opt_ PCWSTR pszDestFile,
+        DWORD dwDestAttribs);
+};
+
+typedef struct _COPY_HOOK_DATA {
+    wchar_t FileName[MAX_PATH];
+} COPY_HOOK_DATA, *PCOPY_HOOK_DATA;
