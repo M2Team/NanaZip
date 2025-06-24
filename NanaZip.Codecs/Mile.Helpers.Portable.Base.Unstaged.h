0bc6d1f32d1b3eef3429368a83edb657bc77e4bd
MouriNaruto <Kenji.Mouri@outlook.com>
2025-06-24 20:49:53 +0800

    Add MileDecodeLeb128 function to Mile.Helpers.Portable.Base.Unstaged.

--- NanaZip.Codecs/Mile.Helpers.Portable.Base.Unstaged.h
+++ NanaZip.Codecs/Mile.Helpers.Portable.Base.Unstaged.h
@@ -148,4 +148,25 @@ EXTERN_C MO_INT64 MileSequenceCompare64(
     _In_ MO_UINT64 Left,
     _In_ MO_UINT64 Right);
 
+/**
+ * @brief Decodes a LEB128-encoded value from the specified memory address.
+ * @param BaseAddress The memory address to read. The caller must ensure that
+ *                    the memory address is valid with at least
+ *                    ((MaximumBits + 6) / 7) bytes of readable memory.
+ * @param MaximumBits The maximum number of bits for the decoded value. This
+ *                    must be between 1 and 64,
+ * @param SignedMode If true, the value is treated as a signed integer; otherwise,
+ *                   the value is treated as an unsigned integer.
+ * @param OutputValue The memory address to store the decoded value. The caller
+ *                    must ensure that the memory address is valid with at least
+ *                    8 bytes of writable memory.
+ * @return The number of processed bytes, or 0 if the decoding failed due to
+ *         invalid parameters, overflow, or exceeding the maximum bits.
+ */
+EXTERN_C MO_UINT8 MileDecodeLeb128(
+    _In_ MO_CONSTANT_POINTER BaseAddress,
+    _In_ MO_UINT8 MaximumBits,
+    _In_ MO_BOOL SignedMode,
+    _Out_ PMO_UINT64 OutputValue);
+
 #endif // !MILE_PORTABLE_HELPERS_BASE_UNSTAGED
