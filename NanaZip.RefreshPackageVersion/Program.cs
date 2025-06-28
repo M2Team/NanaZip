fc6097c4ceac60af34cf263702d6d93722e6b721
MouriNaruto <Kenji.Mouri@outlook.com>
2025-06-28 21:24:43 +0800

    Make NanaZip.RefreshPackageVersion tool to use x-generate to define language resources in AppX manifest.

--- NanaZip.RefreshPackageVersion/Program.cs
+++ NanaZip.RefreshPackageVersion/Program.cs
@@ -152,15 +152,7 @@ namespace NanaZip.RefreshPackageVersion
 
             string NewContent = string.Empty;
             {
-                DirectoryInfo Folder = new DirectoryInfo(string.Format(
-                    @"{0}\NanaZipPackage\Strings",
-                    RepositoryRoot));
-                foreach (var item in Folder.GetDirectories())
-                {
-                    NewContent += string.Format(
-                        "    <Resource Language=\"{0}\" />\r\n",
-                        item.Name);
-                }
+                NewContent += "    <Resource Language=\"x-generate\"/>\r\n";
 
                 int[] Scales = [100, 125, 150, 200, 400];
                 foreach (var Scale in Scales)
