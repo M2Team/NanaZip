66e5a3dd81db40119742adb41c667da16d575830
Dongle <29563098+dongle-the-gadget@users.noreply.github.com>
2025-07-06 02:10:40 +0700

    Add pages

---
+++ NanaZip.Modern/InformationPage.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "InformationPage.h"
+#if __has_include("InformationPage.g.cpp")
+#include "InformationPage.g.cpp"
+#endif
+
+namespace winrt::NanaZip::Modern::implementation
+{
+    DEPENDENCY_PROPERTY_SOURCE_BOX_WITHDEFAULT(
+        Text,
+        winrt::hstring,
+        winrt::NanaZip::Modern::implementation::InformationPage,
+        winrt::NanaZip::Modern::InformationPage,
+        L""
+    );
+
+    void InformationPage::CloseButtonClickedHandler(winrt::IInspectable const&, winrt::RoutedEventArgs const&)
+    {
+    }
+}
---
+++ NanaZip.Modern/InformationPage.h
@@ -0,0 +1,37 @@
+﻿#pragma once
+
+#include "InformationPage.g.h"
+#include "ControlMacros.h"
+#include <Mile.Helpers.CppWinRT.h>
+
+namespace winrt
+{
+    using namespace Windows::Foundation;
+    using namespace Windows::UI::Xaml;
+}
+
+namespace winrt::NanaZip::Modern::implementation
+{
+    struct InformationPage : InformationPageT<InformationPage>
+    {
+        InformationPage() 
+        {
+            // Xaml objects should not call InitializeComponent during construction.
+            // See https://github.com/microsoft/cppwinrt/tree/master/nuget#initializecomponent
+        }
+
+        DEPENDENCY_PROPERTY_HEADER(
+            Text,
+            winrt::hstring
+        );
+
+        void CloseButtonClickedHandler(winrt::IInspectable const& sender, winrt::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::NanaZip::Modern::factory_implementation
+{
+    struct InformationPage : InformationPageT<InformationPage, implementation::InformationPage>
+    {
+    };
+}
