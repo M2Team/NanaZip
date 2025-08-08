93dc2b54801e88a02ee3ccaae93481f12a14df7e
MouriNaruto <Kenji.Mouri@outlook.com>
2025-08-08 21:43:06 +0800

    Update NanaZip.Codecs.Specification.Zealfs.

--- NanaZip.Codecs/NanaZip.Codecs.Specification.Zealfs.h
+++ NanaZip.Codecs/NanaZip.Codecs.Specification.Zealfs.h
@@ -51,8 +51,8 @@ typedef struct _ZEALOS_TIME
     MO_UINT8 Day;
 
     /**
-     * @brief The day of the week. The valid values for this member are 1
-     *        through 7. The 1 is Sunday, 2 is Monday, and so on.
+     * @brief The day of the week. The valid values for this member are 0
+     *        through 6. The 0 is Sunday, 1 is Monday, and so on.
      */
     MO_UINT8 Date;
 
@@ -71,5 +71,89 @@ typedef struct _ZEALOS_TIME
      */
     MO_UINT8 Seconds;
 } ZEALOS_TIME, *PZEALOS_TIME;
+
+/**
+ * @brief The signature for all ZealFS versions, must be 'Z' ascii code.
+ */
+#define ZEALFS_MAGIC 'Z'
+
+/**
+ * @brief The structure of the common partition header for all ZealFS versions.
+ */
+typedef struct _ZEALFS_COMMON_HEADER
+{
+    /**
+     * @brief Must be ZEALFS_MAGIC.
+     */
+    MO_UINT8 Magic;
+
+    /**
+     * @brief Version of the file system.
+     */
+    MO_UINT8 Version;
+} ZEALFS_COMMON_HEADER, *PZEALFS_COMMON_HEADER;
+
+/**
+ * @brief The page size for ZealFS v1.
+ */
+#define ZEALFS_V1_PAGE_SIZE 256
+
+/**
+ * @brief The maximum number of pages for ZealFS v1.
+ */
+#define ZEALFS_V1_MAXIMUM_PAGE_COUNT 256
+
+/**
+ * @brief The bitmap size for ZealFS v1.
+ */
+#define ZEALFS_V1_BITMAP_SIZE (ZEALFS_V1_MAXIMUM_PAGE_COUNT / 8)
+
+/**
+ * @brief The reserved size in partition header for ZealFS v1.
+ */
+#define ZEALFS_V1_RESERVED_SIZE 28
+
+/**
+ * @brief The minimum partition size for ZealFS v1. According to related FUSE
+ *        implementation, the minimum bitmap size seems to be 1 a.k.a. 8 bits
+ *        which for 8 pages.
+ */
+#define ZEALFS_V1_MINIMUM_PARTITION_SIZE (8 * ZEALFS_V1_PAGE_SIZE)
+
+/**
+ * @brief The partition header for ZealFS v1.
+ */
+typedef struct _ZEALFS_V1_HEADER
+{
+    /**
+     * @brief The common partition header for all ZealFS versions.
+     */
+    ZEALFS_COMMON_HEADER Common;
+
+    /**
+     * @brief Number of bytes composing the bitmap. No matter how big the disk
+     *        actually is, the bitmap is always ZEALFS_V1_BITMAP_SIZE bytes big,
+     *        thus we need field to mark the actual number of bytes we will be
+     *        using.
+     */
+    MO_UINT8 BitmapSize;
+
+    /**
+     * @brief Number of free pages.
+     */
+    MO_UINT8 FreePages;
+
+    /**
+     * @brief Bitmap for the free pages. A used page is marked as 1, else 0.
+     *        (256 pages/8-bit = 32)
+     */
+    MO_UINT8 PagesBitmap[ZEALFS_V1_BITMAP_SIZE];
+
+    /**
+     * @brief Reserved bytes, to align the entries and for future use, such as
+     *        extended root directory, volume name, extra bitmap, etc...
+     */
+    MO_UINT8 Reserved[ZEALFS_V1_RESERVED_SIZE];
+} ZEALFS_V1_HEADER, *PZEALFS_V1_HEADER;
 
 #endif // !NANAZIP_CODECS_SPECIFICATION_ZEALFS
