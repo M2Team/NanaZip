62456c060bd3435af7775ef711d866de6d261e04
Tu Dinh <contact@tudinh.xyz>
2025-09-06 16:10:05 +0200

    Rewrite handler/codec restriction infrastructure
    Signed-off-by: Tu Dinh <contact@tudinh.xyz>

---
+++ NanaZip.Shared/Restrictions.h
@@ -0,0 +1,20 @@
+﻿/*
+ * PROJECT:    NanaZip
+ * FILE:       Restrictions.h
+ * PURPOSE:    Definition for handler restriction policies
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: dinhngtu (contact@tudinh.xyz)
+ */
+
+#pragma once
+
+#ifdef Z7_SFX
+#error "This file cannot be used in SFX"
+#endif
+
+#include <Windows.h>
+
+EXTERN_C bool NanaZipIsHandlerAllowedA(PCSTR HandlerName);
+EXTERN_C bool NanaZipIsCodecAllowedA(PCSTR CodecName);
