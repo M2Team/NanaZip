904edf40a5c3e40460c452fbc2257d3b3f6a5b50
Tu Dinh <contact@tudinh.xyz>
2025-09-06 16:10:49 +0200

    Console: Report loaded handlers and codecs
    Signed-off-by: Tu Dinh <contact@tudinh.xyz>

--- NanaZip.Universal/SevenZip/CPP/7zip/UI/Console/Main.cpp
+++ NanaZip.Universal/SevenZip/CPP/7zip/UI/Console/Main.cpp
@@ -249,7 +249,7 @@ static void ShowProgInfo(CStdOutStream *so)
     return;
 
   *so
-  
+
   /*
   #ifdef __DATE__
       << " " << __DATE__
@@ -289,7 +289,7 @@ static void ShowProgInfo(CStdOutStream *so)
       << ".1"
     #endif
   #endif
-    
+
     #if defined(__ARM_ARCH_PROFILE) && \
         (   __ARM_ARCH_PROFILE >= 'A' && __ARM_ARCH_PROFILE <= 'Z' \
          || __ARM_ARCH_PROFILE >= 65  && __ARM_ARCH_PROFILE <= 65 + 25)
@@ -337,7 +337,7 @@ static void ShowProgInfo(CStdOutStream *so)
       *so << " Files=" << off_t_size * 8 << "-bit";
   }
   #endif
-  
+
   {
     const UInt32 numCpus = NWindows::NSystem::GetNumberOfProcessors();
     *so << " Threads:" << numCpus;
@@ -367,11 +367,11 @@ static void ShowProgInfo(CStdOutStream *so)
      << " CRC32"
   #endif
 
-  
+
   #if (defined MY_CPU_X86_OR_AMD64 || defined(MY_CPU_ARM_OR_ARM64))
   if (CPU_IsSupported_AES()) *so << ",AES";
   #endif
-  
+
   #ifdef MY_CPU_ARM_OR_ARM64
   if (CPU_IsSupported_CRC32()) *so << ",CRC32";
   #if defined(_WIN32)
@@ -461,7 +461,7 @@ static int WarningsCheck(HRESULT result, const CCallbackConsoleBase &callback,
     bool showHeaders)
 {
   int exitCode = NExitCode::kSuccess;
-  
+
   if (callback.ScanErrors.Paths.Size() != 0)
   {
     if (se)
@@ -474,7 +474,7 @@ static int WarningsCheck(HRESULT result, const CCallbackConsoleBase &callback,
     }
     exitCode = NExitCode::kWarning;
   }
-  
+
   if (result != S_OK || errorInfo.ThereIsError())
   {
     if (se)
@@ -532,7 +532,7 @@ static int WarningsCheck(HRESULT result, const CCallbackConsoleBase &callback,
     }
     exitCode = NExitCode::kWarning;
   }
-  
+
   return exitCode;
 }
 
@@ -564,7 +564,7 @@ static void PrintTime(const char *s, UInt64 val, UInt64 total)
   *g_StdStream << '.';
   UInt32 ms = (UInt32)(val - (sec * kFreq)) / (kFreq / 1000);
   PrintNum(ms, 3, '0');
-  
+
   while (val > ((UInt64)1 << 56))
   {
     val >>= 1;
@@ -627,7 +627,7 @@ static void PrintStat()
   NTime::GetCurUtc_FiTime(curTimeFT);
 
   #ifndef UNDER_CE
-  
+
 Z7_DIAGNOSTIC_IGNORE_CAST_FUNCTION
 
   PROCESS_MEMORY_COUNTERS m;
@@ -677,11 +677,11 @@ Z7_DIAGNOSTIC_IGNORE_CAST_FUNCTION
   UInt64 userTime = GetTime64(userTimeFT);
 
   UInt64 totalTime = curTime - creationTime;
-  
+
   PrintTime("Kernel ", kernelTime, totalTime);
 
   const UInt64 processTime = kernelTime + userTime;
-  
+
   #ifndef UNDER_CE
   if (cycleDefined)
   {
@@ -704,12 +704,12 @@ Z7_DIAGNOSTIC_IGNORE_CAST_FUNCTION
     *g_StdStream << " MHz";
   }
   #endif
-  
+
   PrintTime("Process", processTime, totalTime);
   #ifndef UNDER_CE
   if (memDefined) PrintMemUsage("Virtual ", m.PeakPagefileUsage);
   #endif
-  
+
   PrintTime("Global ", totalTime, totalTime);
   #ifndef UNDER_CE
   if (memDefined) PrintMemUsage("Physical", m.PeakWorkingSetSize);
@@ -734,7 +734,7 @@ static void PrintTime(const char *s, UInt64 val, UInt64 total_us, UInt64 kFreq)
 
   {
     UInt64 sec, ms;
-    
+
     if (kFreq == 0)
     {
       sec = val / 1000000;
@@ -745,12 +745,12 @@ static void PrintTime(const char *s, UInt64 val, UInt64 total_us, UInt64 kFreq)
       sec = val / kFreq;
       ms = (UInt32)((val - (sec * kFreq)) * 1000 / kFreq);
     }
-    
+
     PrintNum(sec, 6);
     *g_StdStream << '.';
     PrintNum(ms, 3, '0');
   }
-  
+
   if (total_us == 0)
     return;
 
@@ -860,7 +860,7 @@ int Main2(
   */
 
   UStringVector commandStrings;
-  
+
   #ifdef _WIN32
   NCommandLineParser::SplitCommandLine(GetCommandLineW(), commandStrings);
   #else
@@ -901,7 +901,39 @@ int Main2(
     // *g_StdStream << kVersionString;
     *g_StdStream <<
       "NanaZip" PROG_POSTFIX_2 " "
-      MILE_PROJECT_VERSION_UTF8_STRING " (" MY_CPU_NAME ")";
+      MILE_PROJECT_VERSION_UTF8_STRING " (" MY_CPU_NAME ")\n\n";
+
+    CCodecs *Codecs = new CCodecs;
+    HRESULT Result = Codecs->Load();
+    if (Result != S_OK)
+    {
+        return 0;
+    }
+
+    CObjectVector<CCodecInfoUser> CodecInfo;
+    Codecs->Get_CodecsInfoUser_Vector(CodecInfo);
+
+    *g_StdStream << "Loaded formats: ";
+    for (unsigned int i = 0; i < Codecs->Formats.Size(); i++)
+    {
+        if (i > 0)
+        {
+            *g_StdStream << ", ";
+        }
+        *g_StdStream << Codecs->Formats[i].Name;
+    }
+    *g_StdStream << "\n";
+
+    *g_StdStream << "Loaded codecs: ";
+    for (unsigned int i = 0; i < CodecInfo.Size(); i++)
+    {
+        if (i > 0)
+        {
+            *g_StdStream << ", ";
+        }
+        *g_StdStream << CodecInfo[i].Name;
+    }
+    *g_StdStream << "\n";
     // **************** NanaZip Modification End ****************
     return 0;
   }
@@ -925,7 +957,7 @@ int Main2(
   CStdOutStream *percentsStream = NULL;
   if (options.Number_for_Percents != k_OutStream_disabled)
     percentsStream = (options.Number_for_Percents == k_OutStream_stderr) ? &g_StdErr : &g_StdOut;
-  
+
   if (options.HelpMode)
   {
     ShowCopyrightAndHelp(g_StdStream, true);
@@ -943,7 +975,7 @@ int Main2(
 
   {
     int cp = options.ConsoleCodePage;
-    
+
     int stdout_cp = cp;
     int stderr_cp = cp;
     int stdin_cp = cp;
@@ -974,7 +1006,7 @@ int Main2(
       #endif
     }
     */
-    
+
     if (stdout_cp != -1) g_StdOut.CodePage = stdout_cp;
     if (stderr_cp != -1) g_StdErr.CodePage = stderr_cp;
     if (stdin_cp != -1) g_StdIn.CodePage = stdin_cp;
@@ -991,15 +1023,15 @@ int Main2(
   if (percentsStream)
   {
     #ifdef _WIN32
-    
+
     #if !defined(UNDER_CE)
     CONSOLE_SCREEN_BUFFER_INFO consoleInfo;
     if (GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &consoleInfo))
       consoleWidth = (USHORT)consoleInfo.dwSize.X;
     #endif
-    
+
     #else
-    
+
 #if !defined(__sun)
     struct winsize w;
     if (ioctl(0, TIOCGWINSZ, &w) == 0)
@@ -1060,13 +1092,13 @@ int Main2(
     if (!codecs->FindFormatForArchiveType(options.ExcludedArcTypes[k], tempIndices)
         || tempIndices.Size() != 1)
       throw kUnsupportedArcTypeMessage;
-    
-    
-    
+
+
+
     excludedFormats.AddToUniqueSorted(tempIndices[0]);
     // excludedFormats.Sort();
   }
-  
+
   #ifdef Z7_EXTERNAL_CODECS
   if (isExtractGroupCommand
       || options.Command.IsFromUpdateGroup()
@@ -1079,13 +1111,13 @@ int Main2(
   HRESULT hresultMain = S_OK;
 
   // bool showStat = options.ShowTime;
-  
+
   /*
   if (!options.EnableHeaders ||
       options.TechMode)
     showStat = false;
   */
-  
+
 
   if (options.Command.CommandType == NCommandType::kInfo)
   {
@@ -1106,7 +1138,7 @@ int Main2(
     #endif
 
     so << endl << "Formats:" << endl;
-    
+
     const char * const kArcFlags = "KSNFMGOPBELHXCc+a+m+r+";
     const char * const kArcTimeFlags = "wudn";
     const unsigned kNumArcFlags = (unsigned)strlen(kArcFlags);
@@ -1123,7 +1155,7 @@ int Main2(
       #endif
 
       so << (char)(arc.UpdateEnabled ? 'C' : ' ');
-      
+
       {
         unsigned b;
         for (b = 0; b < kNumArcFlags; b++)
@@ -1139,12 +1171,12 @@ int Main2(
         so << arc.Get_DefaultTimePrec();
         so << ' ';
       }
-      
+
       so << ' ';
       PrintString(so, arc.Name, 8);
       so << ' ';
       UString s;
-      
+
       FOR_VECTOR (t, arc.Exts)
       {
         if (t != 0)
@@ -1158,10 +1190,10 @@ int Main2(
           s.Add_Char(')');
         }
       }
-      
+
       PrintString(so, s, 13);
       so << ' ';
-      
+
       if (arc.SignatureOffset != 0)
         so << "offset=" << arc.SignatureOffset << ' ';
 
@@ -1173,7 +1205,7 @@ int Main2(
           so << "  ||  ";
 
         const CByteBuffer &sig = arc.Signatures[si];
-        
+
         for (size_t j = 0; j < sig.Size(); j++)
         {
           if (j != 0)
@@ -1205,7 +1237,7 @@ int Main2(
         so << ' ';
       else
         so << cod.NumStreams;
-      
+
       so << (char)(cod.CreateEncoder ? 'E' : ' ');
       so << (char)(cod.CreateDecoder ? 'D' : ' ');
       so << (char)(cod.IsFilter      ? 'F' : ' ');
@@ -1229,7 +1261,7 @@ int Main2(
         so << ' ';
       else
         so << numStreams;
-      
+
       so << (char)(codecs->GetCodec_EncoderIsAssigned(j) ? 'E' : ' ');
       so << (char)(codecs->GetCodec_DecoderIsAssigned(j) ? 'D' : ' ');
       {
@@ -1249,7 +1281,7 @@ int Main2(
     }
 
     #endif
-    
+
 
     so << endl << "Hashers:" << endl; //  << " L Size       ID Name" << endl;
 
@@ -1264,7 +1296,7 @@ int Main2(
     }
 
     #ifdef Z7_EXTERNAL_CODECS
-    
+
     numMethods = _externalCodecs.GetHashers->GetNumHashers();
     for (UInt32 j = 0; j < numMethods; j++)
     {
@@ -1276,7 +1308,7 @@ int Main2(
     }
 
     #endif
-    
+
   }
   else if (options.Command.CommandType == NCommandType::kBenchmark)
   {
@@ -1305,7 +1337,7 @@ int Main2(
     else
     {
       CExtractScanConsole scan;
-      
+
       scan.Init(options.EnableHeaders ? g_StdStream : NULL,
           g_ErrStream, percentsStream,
           options.DisablePercents);
@@ -1363,7 +1395,7 @@ int Main2(
 
       ecs->LogLevel = options.LogLevel;
       ecs->PercentsNameLevel = percentsNameLevel;
-      
+
       if (percentsStream)
         ecs->SetWindowWidth(consoleWidth);
 
@@ -1379,12 +1411,12 @@ int Main2(
 
       CExtractOptions eo;
       (CExtractOptionsBase &)eo = options.ExtractOptions;
-      
+
       eo.StdInMode = options.StdInMode;
       eo.StdOutMode = options.StdOutMode;
       eo.YesToAll = options.YesToAll;
       eo.TestMode = options.Command.IsTestCommand();
-      
+
       #ifndef Z7_SFX
       eo.Properties = options.Properties;
       #endif
@@ -1400,7 +1432,7 @@ int Main2(
         ThrowException_if_Error(hb.SetMethods(EXTERNAL_CODECS_VARS_L options.HashMethods));
         // hb.Init();
       }
-      
+
       hresultMain = Extract(
           // EXTERNAL_CODECS_VARS_L
           codecs,
@@ -1412,7 +1444,7 @@ int Main2(
           eo,
           ecs, ecs, ecs,
           hashCalc, errorMessage, stat);
-      
+
       ecs->ClosePercents();
 
       if (!errorMessage.IsEmpty())
@@ -1430,7 +1462,7 @@ int Main2(
       if (so)
       {
         *so << endl;
-        
+
         if (ecs->NumTryArcs > 1)
         {
           *so << "Archives: " << ecs->NumTryArcs << endl;
@@ -1444,19 +1476,19 @@ int Main2(
         if (so)
           *so << "Can't open as archive: " << ecs->NumCantOpenArcs << endl;
       }
-      
+
       if (ecs->NumArcsWithError != 0)
       {
         isError = true;
         if (so)
           *so << "Archives with Errors: " << ecs->NumArcsWithError << endl;
       }
-      
+
       if (so)
       {
         if (ecs->NumArcsWithWarnings != 0)
           *so << "Archives with Warnings: " << ecs->NumArcsWithWarnings << endl;
-        
+
         if (ecs->NumOpenArcWarnings != 0)
         {
           *so << endl;
@@ -1464,7 +1496,7 @@ int Main2(
             *so << "Warnings: " << ecs->NumOpenArcWarnings << endl;
         }
       }
-      
+
       if (ecs->NumOpenArcErrors != 0)
       {
         isError = true;
@@ -1478,7 +1510,7 @@ int Main2(
 
       if (isError)
         retCode = NExitCode::kFatalError;
-      
+
       if (so) {
       if (ecs->NumArcsWithError != 0 || ecs->NumFileErrors != 0)
       {
@@ -1500,7 +1532,7 @@ int Main2(
           *so << "Alternate Streams: " << stat.NumAltStreams << endl;
           *so << "Alternate Streams Size: " << stat.AltStreams_UnpackSize << endl;
         }
-        
+
         *so
           << "Size:       " << stat.UnpackSize << endl
           << "Compressed: " << stat.PackSize << endl;
@@ -1516,7 +1548,7 @@ int Main2(
     {
       UInt64 numErrors = 0;
       UInt64 numWarnings = 0;
-      
+
       // options.ExtractNtOptions.StoreAltStreams = true, if -sns[-] is not definmed
 
       CListOptions lo;
@@ -1547,7 +1579,7 @@ int Main2(
       if (options.EnableHeaders)
         if (numWarnings > 0)
           g_StdOut << endl << "Warnings: " << numWarnings << endl;
-      
+
       if (numErrors > 0)
       {
         if (options.EnableHeaders)
@@ -1626,7 +1658,7 @@ int Main2(
     CHashCallbackConsole callback;
     if (percentsStream)
       callback.SetWindowWidth(consoleWidth);
-  
+
     callback.Init(g_StdStream, g_ErrStream, percentsStream, options.DisablePercents);
     callback.PrintHeaders = options.EnableHeaders;
     callback.PrintFields = options.ListFields;
