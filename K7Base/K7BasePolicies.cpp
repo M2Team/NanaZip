16cf6ad4f0bd68c7586df6b939c789680a526bc3
MouriNaruto <Kenji.Mouri@outlook.com>
2025-12-23 23:20:30 +0800

    Add K7BasePolicies to K7Base.

---
+++ K7Base/K7BasePolicies.cpp
@@ -0,0 +1,12 @@
+﻿/*
+ * PROJECT:    NanaZip Platform Base Library (K7Base)
+ * FILE:       K7BasePolicies.cpp
+ * PURPOSE:    Implementation for NanaZip Platform Base Policies Interfaces
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: dinhngtu (contact@tudinh.xyz)
+ *             MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "K7BasePolicies.h"
---
+++ K7Base/K7BasePolicies.h
@@ -0,0 +1,28 @@
+﻿/*
+ * PROJECT:    NanaZip Platform Base Library (K7Base)
+ * FILE:       K7BasePolicies.h
+ * PURPOSE:    Definition for NanaZip Platform Base Policies Interfaces
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: dinhngtu (contact@tudinh.xyz)
+ *             MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef K7_BASE_POLICIES
+#define K7_BASE_POLICIES
+
+#include <Mile.Mobility.Portable.Types.h>
+#ifndef MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <sal.h>
+#endif // !MILE_MOBILITY_ENABLE_MINIMUM_SAL
+
+// Workaround for Windows SDK, which defines these types in #ifndef VOID block.
+// That design is terrible, but needs to have a workaround.
+#ifdef VOID
+typedef char CHAR;
+typedef short SHORT;
+typedef long LONG;
+#endif // VOID
+
+#endif // !K7_BASE_POLICIES
