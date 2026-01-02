1b57c10174b290fb3c732dc4f26b87b840b0e0da
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-02 23:12:14 +0800

    Add K7BaseMitigations to K7Base.

---
+++ K7Base/K7BaseMitigations.cpp
@@ -0,0 +1,12 @@
+﻿/*
+ * PROJECT:    NanaZip Platform Base Library (K7Base)
+ * FILE:       K7BaseMitigations.cpp
+ * PURPOSE:    Implementation for NanaZip Platform Base Mitigations Interfaces
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: dinhngtu (contact@tudinh.xyz)
+ *             MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "K7BaseMitigations.h"
---
+++ K7Base/K7BaseMitigations.h
@@ -0,0 +1,28 @@
+﻿/*
+ * PROJECT:    NanaZip Platform Base Library (K7Base)
+ * FILE:       K7BaseMitigations.h
+ * PURPOSE:    Definition for NanaZip Platform Base Mitigations Interfaces
+ *
+ * LICENSE:    The MIT License
+ *
+ * MAINTAINER: dinhngtu (contact@tudinh.xyz)
+ *             MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef K7_BASE_MITIGATIONS
+#define K7_BASE_MITIGATIONS
+
+#include <Mile.Mobility.Portable.Types.h>
+#ifndef MILE_MOBILITY_ENABLE_MINIMUM_SAL
+#include <sal.h>
+#endif // !MILE_MOBILITY_ENABLE_MINIMUM_SAL
+
+// Workaround for Windows SDK, which defines these types in #ifndef VOID block.
+// That design is terrible, but needs to have a workaround.
+#ifdef VOID
+typedef char CHAR;
+typedef short SHORT;
+typedef long LONG;
+#endif // VOID
+
+#endif // !K7_BASE_MITIGATIONS
