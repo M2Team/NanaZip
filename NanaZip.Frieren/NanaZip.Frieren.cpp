1026b940f870142b40088ef8c5c219c40ddad6d9
MouriNaruto <Kenji.Mouri@outlook.com>
2026-01-19 22:43:59 +0800

    Remove unnecessary functions in NanaZip.Frieren.

--- NanaZip.Frieren/NanaZip.Frieren.cpp
+++ NanaZip.Frieren/NanaZip.Frieren.cpp
@@ -10,31 +10,10 @@
 
 #include "NanaZip.Frieren.h"
 
-EXTERN_C VOID WINAPI NanaZipFrierenDarkModeThreadInitialize();
-EXTERN_C VOID WINAPI NanaZipFrierenDarkModeThreadUninitialize();
 EXTERN_C VOID WINAPI NanaZipFrierenDarkModeGlobalInitialize();
-EXTERN_C VOID WINAPI NanaZipFrierenDarkModeGlobalUninitialize();
-
-EXTERN_C HRESULT WINAPI NanaZipFrierenThreadInitialize()
-{
-    ::NanaZipFrierenDarkModeThreadInitialize();
-    return S_OK;
-}
-
-EXTERN_C HRESULT WINAPI NanaZipFrierenThreadUninitialize()
-{
-    ::NanaZipFrierenDarkModeThreadUninitialize();
-    return S_OK;
-}
 
 EXTERN_C HRESULT WINAPI NanaZipFrierenGlobalInitialize()
 {
     ::NanaZipFrierenDarkModeGlobalInitialize();
-    return S_OK;
-}
-
-EXTERN_C HRESULT WINAPI NanaZipFrierenGlobalUninitialize()
-{
-    ::NanaZipFrierenDarkModeGlobalUninitialize();
     return S_OK;
 }
--- NanaZip.Frieren/NanaZip.Frieren.h
+++ NanaZip.Frieren/NanaZip.Frieren.h
@@ -14,31 +14,10 @@
 #include <Windows.h>
 
 /**
- * @brief Initialize NanaZip.Frieren for non-main thread.
- * @return If the function succeeds, it returns S_OK. Otherwise, it returns an
- *         HRESULT error code.
-*/
-EXTERN_C HRESULT WINAPI NanaZipFrierenThreadInitialize();
-
-/**
- * @brief Uninitialize NanaZip.Frieren for non-main thread.
- * @return If the function succeeds, it returns S_OK. Otherwise, it returns an
- *         HRESULT error code.
-*/
-EXTERN_C HRESULT WINAPI NanaZipFrierenThreadUninitialize();
-
-/**
  * @brief Initialize NanaZip.Frieren for main thread.
  * @return If the function succeeds, it returns S_OK. Otherwise, it returns an
  *         HRESULT error code.
 */
 EXTERN_C HRESULT WINAPI NanaZipFrierenGlobalInitialize();
-
-/**
- * @brief Uninitialize NanaZip.Frieren for main thread.
- * @return If the function succeeds, it returns S_OK. Otherwise, it returns an
- *         HRESULT error code.
-*/
-EXTERN_C HRESULT WINAPI NanaZipFrierenGlobalUninitialize();
 
 #endif // !NANAZIP_FRIEREN
