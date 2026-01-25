0ba7c595acf3905d0a1903ab3c901f9ebc0b2074
Tu Dinh <1257909+dinhngtu@users.noreply.github.com>
2026-01-25 07:01:25 +0100

    Fix error handling of NanaZip.Codecs (#812)
    - S_FALSE during Open() should also clean up the handler.
    - GetArchiveProperty should return VT_EMPTY instead of failing when an
      archive is not opened, since it will otherwise interfere with update
      workflows, where UpdateArchive calls GetArchiveProperty on the
      handler's error props.
    
    Signed-off-by: Tu Dinh <contact@tudinh.xyz>

--- NanaZip.Codecs/NanaZip.Codecs.Archive.WebAssembly.cpp
+++ NanaZip.Codecs/NanaZip.Codecs.Archive.WebAssembly.cpp
@@ -341,7 +341,7 @@ namespace NanaZip::Codecs::Archive
 
             } while (false);
 
-            if (FAILED(hr))
+            if (hr != S_OK)
             {
                 this->Close();
             }
@@ -537,14 +537,15 @@ namespace NanaZip::Codecs::Archive
             _In_ PROPID PropId,
             _Inout_ LPPROPVARIANT Value)
         {
-            if (!this->m_IsInitialized)
-            {
-                return S_FALSE;
-            }
-
             if (!Value)
             {
                 return E_INVALIDARG;
+            }
+
+            if (!this->m_IsInitialized)
+            {
+                Value->vt = VT_EMPTY;
+                return S_OK;
             }
 
             switch (PropId)
