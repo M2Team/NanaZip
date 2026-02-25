f9ea9f23445f882ea4555b7db150c16488ff407d
peashooter2 <baozouptu_244597@qq.com>
2026-02-25 19:41:21 +0800

    trying to fix hypothetical BO introduced in v.26.00.

--- NanaZip.Core/SevenZip/CPP/7zip/Bundles/SFXSetup/SfxSetup.cpp
+++ NanaZip.Core/SevenZip/CPP/7zip/Bundles/SFXSetup/SfxSetup.cpp
@@ -78,7 +78,7 @@ static bool ReadDataString(CFSTR fileName, LPCSTR startID,
     {
       if (writeMode)
       {
-        if (pos + signatureEndSize > numBytesPrev)
+        if (pos + signatureEndSize >= numBytesPrev)
           break;
         const Byte b = buffer[pos++];
         if (b == 0)
@@ -89,7 +89,7 @@ static bool ReadDataString(CFSTR fileName, LPCSTR startID,
       }
       else
       {
-        if (pos + signatureStartSize > numBytesPrev)
+        if (pos + signatureStartSize >= numBytesPrev)
           break;
         const Byte b = buffer[pos++];
         if (b == ';' && memcmp(buffer + pos, startID + 1, signatureStartSize) == 0)
