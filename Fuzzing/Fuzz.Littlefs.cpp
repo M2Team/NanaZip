0d032c4ba53784ea66dc3fa8d59ebff8651d51b7
jarlob <jarlob@gmail.com>
2026-05-27 12:40:51 +0300

    Add fuzzing infrastructure

---
+++ Fuzzing/Fuzz.Littlefs.cpp
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:    NanaZip
+ * FILE:       Fuzz.Littlefs.cpp
+ * PURPOSE:    libFuzzer harness for the littlefs handler
+ *
+ * LICENSE:    The MIT License
+ */
+
+#include "NanaZip.Codecs.Fuzz.h"
+
+extern "C" int LLVMFuzzerTestOneInput(
+    const std::uint8_t* Data,
+    std::size_t Size)
+{
+    NanaZip::Fuzz::RunFuzzCase(6, Data, Size);
+    return 0;
+}
---
+++ Fuzzing/Fuzz.Zealfs.cpp
@@ -0,0 +1,17 @@
+﻿/*
+ * PROJECT:    NanaZip
+ * FILE:       Fuzz.Zealfs.cpp
+ * PURPOSE:    libFuzzer harness for the ZealFS handler
+ *
+ * LICENSE:    The MIT License
+ */
+
+#include "NanaZip.Codecs.Fuzz.h"
+
+extern "C" int LLVMFuzzerTestOneInput(
+    const std::uint8_t* Data,
+    std::size_t Size)
+{
+    NanaZip::Fuzz::RunFuzzCase(4, Data, Size);
+    return 0;
+}
